6ceae81bca28e1d81c3affc76d01d03d080be92f
Piyush Singh <epiyusi@fulmar.hw.etv.eld.ericsson.se>
2015-11-26 14:16:54 +0000

    Work in progress checkpoint

--- min-char-rnn.py
+++ min-char-rnn.py
@@ -45,11 +45,6 @@ class networkWeights:
 
     def __init__(self, hidden_size, input_size):
 
-        #print "__init__ LstmParam"
-
-        #self.hidden_size = hidden_size
-        #self.input_size  = input_size
-
         ##
         # Weight matrices describe the linear fransformation from 
         # input space to output space.
@@ -61,14 +56,13 @@ class networkWeights:
         self.by  = np.zeros((input_size  , 1)) # output bias
 
 
-
         # Memory variables for AdaGrad
         self.mWxh, self.mWhh, self.mWhy = np.zeros_like(self.Wxh), np.zeros_like(self.Whh), np.zeros_like(self.Why)
         self.mbh, self.mby = np.zeros_like(self.bh), np.zeros_like(self.by) # memory variables for Adagrad
 
 
 
-    def weightUpdate(self, grads, learning_rate = 1e-1):
+    def adagradUpdate(self, grads, learning_rate = 1e-1):
         """
         Weight update using Adagrad 
         """
@@ -143,7 +137,9 @@ class RnnCell:
 
     
     def backwardPass(self, dy, dh_1, hs_1, weights):
-        # notice that diff_s is carried along the constant error carousel
+        """
+        Propagate error from output to inpits
+        """
 
         DP = np.dot
 
@@ -211,9 +207,7 @@ class Rnn:
         hs = {}
         hs[-1] = np.copy(hprev)
 
-        # Initialise gradients variables
-        grads   = networkGradients(self.hidden_size, self.input_size)
-       
+
         ###
         
         ####
@@ -238,7 +232,15 @@ class Rnn:
         
 
         ####
+
+
         # backward pass: compute gradients going backwards
+
+        # Initialise gradients variables
+        grads   = networkGradients(self.hidden_size, self.input_size)
+       
+
+       
         dh_1 = np.zeros_like(self.CELLS[0].hs)
         
         for t in reversed(xrange(self.rnn_depth)):
@@ -273,7 +275,7 @@ class Rnn:
 
 
 
-def sample2(hprev, seed_ix, n, hidden_size, input_size, weights):
+def sample(hprev, seed_ix, n, hidden_size, input_size, weights):
     """ 
     sample a sequence of integers from the model 
     h is memory state, seed_ix is seed letter for first time step
@@ -311,75 +313,6 @@ def sample2(hprev, seed_ix, n, hidden_size, input_size, weights):
 
 
 
-def sample(h, seed_ix, n):
-    """ 
-    sample a sequence of integers from the model 
-    h is memory state, seed_ix is seed letter for first time step
-    """
-    x = np.zeros((input_size, 1))
-    x[seed_ix] = 1
-    ixes = []
-    for t in xrange(n):
-        h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
-        y = np.dot(Why, h) + by
-        p = np.exp(y) / np.sum(np.exp(y))
-        ix = np.random.choice(range(input_size), p=p.ravel())
-        x = np.zeros((input_size, 1))
-        x[ix] = 1
-        ixes.append(ix)
-    return ixes
-
-
-
-
-
-def lossFun(inputs, targets, hprev):
-  """
-  inputs,targets are both list of integers.
-  hprev is Hx1 array of initial hidden state
-  returns the loss, gradients on model parameters, and last hidden state
-  """
-  xs, hs, ys, ps = {}, {}, {}, {}
-  hs[-1] = np.copy(hprev)
-  loss = 0
-
-  ####
-  # forward pass
-  for t in xrange(len(inputs)):
-    
-    xs[t] = np.zeros((input_size,1)) # encode in 1-of-k representation
-    xs[t][inputs[t]] = 1
-
-    hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
-    ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next chars
-    ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
-    
-    loss += -np.log(ps[t][targets[t],0]) # softmax (cross-entropy loss)
-  
-  ####
-  # backward pass: compute gradients going backwards
-  dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-  dbh, dby = np.zeros_like(bh), np.zeros_like(by)
-  dhnext = np.zeros_like(hs[0])
-  
-  for t in reversed(xrange(len(inputs))):
-    dy     = np.copy(ps[t])
-    dy[targets[t]] -= 1 # backprop into y
-    dWhy  += np.dot(dy, hs[t].T)
-    dby   += dy
-    dh     = np.dot(Why.T, dy) + dhnext # backprop into h
-    dhraw  = (1 - hs[t] * hs[t]) * dh # backprop through tanh nonlinearity
-    dbh   += dhraw
-    dWxh  += np.dot(dhraw, xs[t].T)
-    dWhh  += np.dot(dhraw, hs[t-1].T)
-    dhnext = np.dot(Whh.T, dhraw)
-
-  for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-    np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
-  
-  return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs)-1]
-
-
 
 # gradient checking
 def gradCheck(inputs, target, hprev):
@@ -409,91 +342,83 @@ def gradCheck(inputs, target, hprev):
 
 
 
-###### Main function here
-###### Main function here
-###### Main function here
+## Entry point
+if __name__ == "__main__":
+
+    # data I/O
+    data  = open('input2.txt', 'r').read() # should be simple plain text file
+    chars = list(set(data))
+
+    data_size, input_size = len(data), len(chars)
+    print 'data has %d characters, %d unique.' % (data_size, input_size)
+    char_to_ix = { ch:i for i,ch in enumerate(chars) }
+    ix_to_char = { i:ch for i,ch in enumerate(chars) }
+
+    # Hyperparameters
+    hidden_size   = 100  # size of hidden layer of neurons
+    seq_length    = 25    # number of steps to unroll the RNN for
+    learning_rate = 1e-1
+
+    # RNN model parameters
+    weights = networkWeights(hidden_size, input_size)
 
 
-np.random.seed(3)
-
-# data I/O
-data  = open('input2.txt', 'r').read() # should be simple plain text file
-chars = list(set(data))
-
-data_size, input_size = len(data), len(chars)
-print 'data has %d characters, %d unique.' % (data_size, input_size)
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
-
-# hyperparameters
-hidden_size = 100 # size of hidden layer of neurons
-seq_length = 25 # number of steps to unroll the RNN for
-learning_rate = 1e-1
-
-# model parameters
-#PARAM = networkWeights(hidden_size, input_size)
-#pdb.set_trace()
-PARAM = networkWeights(hidden_size, input_size)
-
-
-
-
-
-
-
-n, p = 0, 0
-smooth_loss = -np.log(1.0/input_size)*seq_length # loss at iteration 0
-
-keepGoing = True
-
-
-rnnObj = Rnn(seq_length, hidden_size, input_size)
-
-# Main loop
-while keepGoing:
-
-    # prepare inputs (we're sweeping from left to right in steps seq_length long)
-    if p+seq_length+1 >= len(data) or n == 0: 
-        hprev = np.zeros((hidden_size,1)) # reset RNN memory
-        p = 0 # go from start of data
-
-    # Prepare inputs and targets
-    inputs  = [char_to_ix[ch] for ch in data[ p  :p+seq_length   ]]
-    targets = [char_to_ix[ch] for ch in data[ p+1:p+seq_length+1 ]]
-
-    # sample from the model now and then
-    if n % 100 == 0:
-        seedIn = inputs[0]
-        sample_ix = sample2(hprev, inputs[0], 200, hidden_size, input_size, PARAM)
-        txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-        print '----\n %s \n----' % (txt, )
-
-    # forward seq_length characters through the net and fetch gradient
-    #pdb.set_trace()
-    hprev, grads = rnnObj.lossFunModif(inputs, targets, hprev, PARAM)
-    loss = rnnObj.loss
-
-    #rnnObj.resetGrads()
-
-    # Smooth and log message print
-    smooth_loss = smooth_loss * 0.999 + loss * 0.001
-    if n % 100 == 0: print 'iter %d, loss: %f' % (n, smooth_loss) # print progress
+    n, p = 0, 0
     
-    # Adagrad weight update
-    PARAM.weightUpdate(grads)
+    # Initialise smoothed loss
+    smooth_loss = -np.log(1.0/input_size)*seq_length # loss at iteration 0
+
+    keepGoing = True
+
+    # Create instance of RNN
+    rnnObj = Rnn(seq_length, hidden_size, input_size)
+
+    # Main loop
+    while keepGoing:
+
+        # prepare inputs (we're sweeping from left to right in steps seq_length long)
+        if p+seq_length+1 >= len(data) or n == 0: 
+            hprev = np.zeros((hidden_size,1)) # reset RNN memory
+            p = 0 # go from start of data
+
+        # Prepare inputs and targets
+        inputs  = [char_to_ix[ch] for ch in data[ p  :p+seq_length   ]]
+        targets = [char_to_ix[ch] for ch in data[ p+1:p+seq_length+1 ]]
+
+        # sample from the model now and then
+        if n % 100 == 0:
+            seedIn = inputs[0]
+            sample_ix = sample(hprev, inputs[0], 200, hidden_size, input_size, weights)
+            txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+            print '----\n %s \n----' % (txt, )
+
+        # forward seq_length characters through the net and fetch gradient
+        hprev, grads = rnnObj.lossFunModif(inputs, targets, hprev, weights)
+        loss = rnnObj.loss
+
+        #rnnObj.resetGrads()
+
+        # Smooth and log message print
+        smooth_loss = smooth_loss * 0.999 + loss * 0.001
+        if n % 100 == 0: print 'iter %d, loss: %f' % (n, smooth_loss) # print progress
+        
+        # Adagrad weight update
+        weights.adagradUpdate(grads)
 
 
-    # move data pointer
-    p += seq_length
+        # move data pointer
+        p += seq_length
 
-    # iteration counter 
-    n += 1
+        # iteration counter 
+        n += 1
 
-    # Terminate
-    if (n == 300):
-        keepGoing = False
+        # Terminate
+        keepGoing = False if (n == 300) else True
 
 
+
+
+    
 
 
 #
